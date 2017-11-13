e7bc492e9a92941c0cdc8ad3af2b8b437ed6d708
Ryan Kaplan <ryan_kaplan@me.com>
2017-11-13 13:32:04 -0800

    Creates new react class "Time".

--- client/index.js
+++ client/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
-function Clock(props) {
-  return <div>Time is currently {props.date.toLocaleTimeString()}.</div>
+function Time(props) {
+  return <div>The time is {props.date.toLocaleTimeString()}</div>
 }
 
 function tick() {
-  ReactDOM.render(<Clock date={new Date()} />, document.querySelector('#app'))
+  render(<Time date={new Date()} />, document.querySelector('#app'))
 }
 
 setInterval(tick, 1000)
