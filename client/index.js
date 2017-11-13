2e28f44ac8e6b7f2a37daca8989e7354db823fb6
Ryan Kaplan <ryan_kaplan@me.com>
2017-11-13 13:16:16 -0800

    Adds index.html, package.json, app.js

--- client/index.js
+++ client/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 
-const tick = () => {
-  const time = <div>The time is {new Date().toLocaleTimeString()}.</div>
-  render(time, document.querySelector('#app'))
+function Clock(props) {
+  return <div>Time is currently {props.date.toLocaleTimeString()}.</div>
+}
+
+function tick() {
+  ReactDOM.render(<Clock date={new Date()} />, document.querySelector('#app'))
 }
 
 setInterval(tick, 1000)
